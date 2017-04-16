d16f9b1cbdceef05d6a2373698a0271d6f28069e
asand017 <asand017@ucr.edu>
2017-04-16 14:27:14 -0700

    first attempts at passing in java files

--- Typecheck.java
+++ Typecheck.java
@@ -5,11 +5,26 @@
  *
  */
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+import java.io.FileInputStream;
 
 public class Typecheck{
 
    public static void main(String args[]){
-	System.out.println("Program type checked successfully");
+	
+	try{
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String s;
+		while((s = in.readLine()) == ){
+			//String s = in.readLine();
+			System.out.println(s);
+			//System.out.println("Program type checked successfully");
+		}
+	} catch(Exception e){
+	 	e.printStackTrace();
+	}
    }
 
 }
