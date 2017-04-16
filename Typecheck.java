53b15c103c3719f631683d41f46e7ab250d314db
asand017 <asand017@ucr.edu>
2017-04-16 13:47:38 -0700

    opened tar files

---
+++ Typecheck.java
@@ -0,0 +1,15 @@
+/**
+ * Team Members: Aaron Sanders, Cristian Ibarra, Leon Peng
+ * 
+ * Typecheck.java
+ *
+ */
+
+
+public class Typecheck{
+
+   public static void main(String args[]){
+	System.out.println("Program type checked successfully");
+   }
+
+}
