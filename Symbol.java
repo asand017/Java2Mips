c896cc5b3029bf56a65ea6cd4109ef6cf5131a0a
asand017 <asand017@ucr.edu>
2017-04-26 20:29:38 -0700

    Add Symbol and Symbol table files

---
+++ Symbol.java
@@ -0,0 +1,27 @@
+/* Team Members: Aaron Sanders, Crisitian Ibarra, Leon Peng
+ *
+ *
+ * Symbol.java
+ * - From texbook, page 109
+ *
+ *
+ */
+
+package Symbol;
+
+public class Symbol {
+	public String toString();
+	public static Symbol symbol(String s);
+}
+
+public class Table {
+	
+	public Table();
+	public void put(Symbol key, Object value);
+	public Object get(Symbol key);
+	public void beginScope();
+	public void endScope();
+	public java.util.Enumeration keys();
+
+	
+}
