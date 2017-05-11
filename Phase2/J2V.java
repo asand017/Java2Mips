e6b2b0a7a5ec4fd10429f431070b399d21782676
Cristian Ibarra <cibar007@bolt.cs.ucr.edu>
2017-05-11 10:24:26 -0700

    Extended main file to visitor

--- Phase2/J2V.java
+++ Phase2/J2V.java
@@ -7,6 +7,22 @@
  *
  */
 
-public class J2V {
+import java.io.BufferedReader;
+import syntaxtree.*;
+import visitor.*;
 
+public class J2V extends DepthFirstVisitor{
+
+	//do all the fun stuff here
+	
+	public static void main(String args[]) {
+		try{
+			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+			//reading in from console input 
+
+			MiniJavaParser parser = new MiniJavaParser(in);
+
+			Goal root = parser.Goal();
+		}
+	}
 }
