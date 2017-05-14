975d99f9ebae5b17467a1b348643bf7e6b6f9dcb
Cristian Ibarra <cibar007@bolt.cs.ucr.edu>
2017-05-14 11:50:05 -0700

    added strings for use in output

--- Phase2/J2V.java
+++ Phase2/J2V.java
@@ -15,6 +15,29 @@ public class J2V extends DepthFirstVisitor{
 
 	//do all the fun stuff here
 	
+	public static String data_label = "";
+	
+	public static String Main = "";
+
+	public class Method {
+		private String[] mthd;
+		public int size = 0;
+		public int index = 0;
+
+		public static void input(String block) {
+			mthd[index] = block;
+			size++;
+			index++;
+		}
+
+		public static void print_method() {
+			for(int i = 0; i < size; i++) {
+				method[i]; //print or otuput to file
+				//new line between both methods
+			}
+		}
+	}
+
 	public static void main(String args[]) {
 		try{
 			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
