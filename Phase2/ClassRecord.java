f66e960ea6131d3579ec3f2a05fe16afcfcabe69
asand017 <asand017@ucr.edu>
2017-05-20 09:49:35 -0700

    added Class Record class file

---
+++ Phase2/ClassRecord.java
@@ -0,0 +1,16 @@
+/*
+ * Team Members: Aaron Sanders, Cristian Ibarra, Leon Peng
+ *
+ * ClassRecord.java
+ *
+ * Class Record class
+ *
+ */
+
+import java.util.*;
+
+class ClassRecord {
+	public static Map class_ref = new HashMap();
+	public static ArrayList<String> vtable = new ArrayList<String>();
+	public static ArrayList<String> classRecord = new ArrayList<String>(); //use ArrayList index for i in 
+}
