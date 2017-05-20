2c326a531495dbe50685c86719d911d00d08ae4e
asand017 <asand017@ucr.edu>
2017-05-20 10:58:33 -0700

    added vtable and changed class record

--- Phase2/ClassRecord.java
+++ Phase2/ClassRecord.java
@@ -9,14 +9,15 @@
 
 import java.util.*;
 
-class ClassRecord {
-	
-	
+class ClassRecord {		
 
 	public static Map class_ref = new HashMap(); // (class_name, ArrayList<String> vtable)
-	public static ArrayList<String> vtable = new ArrayList<String>(); // // (method1, method2,...)
+	//public static ArrayList<String> vtable = new ArrayList<String>(); // // (method1, method2,...)
 	public static ArrayList<Map> classRecord = new ArrayList<Map>(); //use ArrayList index for i in
 	// (class_ref1, class_ref2,....)
 
+	public void add(Map class_reference){//pass in a class_ref to be put into the Class Record
+		classRecord.add(class_reference);
+	}
 	 
 }
--- Phase2/VTable.java
+++ Phase2/VTable.java
@@ -7,5 +7,9 @@
 import java.util.*;
 
 class VTable{
+	public static ArrayList<String> vtable = new ArrayList<String>(); //vtable
 
+	public void add(String method){
+		vtable.add(method);
+	}
 }
