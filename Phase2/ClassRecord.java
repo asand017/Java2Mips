acee84df756b3ee9802751d87d66d073f6c4c659
asand017 <asand017@ucr.edu>
2017-05-20 10:00:42 -0700

    set up ClassRecord logic

--- Phase2/ClassRecord.java
+++ Phase2/ClassRecord.java
@@ -10,7 +10,13 @@
 import java.util.*;
 
 class ClassRecord {
-	public static Map class_ref = new HashMap();
-	public static ArrayList<String> vtable = new ArrayList<String>();
-	public static ArrayList<String> classRecord = new ArrayList<String>(); //use ArrayList index for i in 
+	
+	
+
+	public static Map class_ref = new HashMap(); // (class_name, ArrayList<String> vtable)
+	public static ArrayList<String> vtable = new ArrayList<String>(); // // (method1, method2,...)
+	public static ArrayList<Map> classRecord = new ArrayList<Map>(); //use ArrayList index for i in
+	// (class_ref1, class_ref2,....)
+
+	 
 }
